1fc148644dee487d90e1624124169cdb9a29e8c4
Rafael <ramachado40@gmail.com>
2024-05-14 19:32:52 -0400

    búsqueda de estudiantes

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ async function obtenerDatos(){
         let pie = document.getElementById("pie");
         pie.innerHTML = json.copyRight;
         let nombre = document.getElementById("nombre");
-        nombre.value = json.nombre;
+        nombre.setAttribute("placeholder", json.nombre);
         let saludo = document.getElementById("saludo");
         saludo.innerHTML = json.saludo + ", Rafael";
 
@@ -31,7 +31,7 @@ async function obtenerDatos(){
         let pie = document.getElementById("pie");
         pie.innerHTML = json.copyRight;
         let nombre = document.getElementById("nombre");
-        nombre.value = json.nombre;
+        nombre.setAttribute("placeholder", json.nombre);
         let saludo = document.getElementById("saludo");
         saludo.innerHTML = json.saludo + ", Rafael";
     }else {
@@ -47,7 +47,7 @@ async function obtenerDatos(){
         let pie = document.getElementById("pie");
         pie.innerHTML = json.copyRight;
         let nombre = document.getElementById("nombre");
-        nombre.value = json.nombre;
+        nombre.setAttribute("placeholder", json.nombre);
         let saludo = document.getElementById("saludo");
         saludo.innerHTML = json.saludo + ", Rafael";
     }
@@ -82,8 +82,8 @@ async function estructuraPerfil(){
     descri.innerHTML = "<em>" + index.descripcion + "</em>";
     let nombre = document.getElementById("nombre");
     nombre.innerHTML = index.nombre;
-    }
-    else if( comprueba == "PT"){
+    
+    }else if( comprueba == "PT"){
     const respuesta = await fetch("http://127.0.0.1:5500/reto5/conf/configPT.json");
     const index = await respuesta.json();
 
@@ -141,6 +141,7 @@ async function listarEstudiantes(){
         ima.setAttribute("src", "./reto5/"+nomfuente);
         lis.appendChild(ima);
         lis.appendChild(nombre);
+        lis.setAttribute("class", "estudiante");
         document.getElementById("estudiantes").appendChild(lis);
     });
 }
@@ -185,8 +186,131 @@ async function cargarPerfil(){
 
         }
         )
+    }else{
+        fetch("http://127.0.0.1:5500/reto5/27344078/perfil.json")
+                    .then(function(res) {
+                        return res.json();
+                    })
+                    .then(function(data){
+                       let nombre = document.getElementById("nombre");
+                       nombre.innerHTML = data.nombre
+                       let descrip = document.getElementById("descripcion");
+                       descrip.innerHTML = data.descripcion;
+                       let color = document.getElementById("colorr");
+                       color.innerHTML = data.color;
+                       let lib = document.getElementById("libror");
+                       lib.innerHTML = data.libro;
+                       let music = document.getElementById("musicar");
+                       music.innerHTML = data.musica
+                       let vg = document.getElementById("videojuegosr");
+                       vg.innerHTML = data.video_juego;
+                       let lengua = document.getElementById("lenguajesr");
+                       lengua.innerHTML = "<strong>" + data.lenguajes + "</strong>";
+                       let mail = document.getElementById("mail");
+                       mail.innerHTML = mail.innerHTML.replace("[email]", "<a id='enlace'>"+data.email+"</a>");
+                       let enlace = document.getElementById("enlace");
+                       enlace.setAttribute("href", "mailto:"+data.email)
+                    })
+                let imagen = document.getElementById("fotoPerfil");
+                imagen.setAttribute("src", "./reto5/27344078/CEDULAGrande.jpg");
     }
         
     
 }
 cargarPerfil();
+
+function busqueda(){
+    var padre = document.getElementById("estudiantes");
+    if(document.getElementById("borrame")){
+        var referencia = document.getElementById("borrame");
+        padre.removeChild(referencia);
+    }
+       
+    
+    
+
+    var x =  document.getElementById("nombre").value;
+    var ele2 = x.toUpperCase();
+    const arrEstudiantes = document.getElementsByClassName("estudiante");
+    var contador = 0
+    var limite = arrEstudiantes.length;
+    Array.from(arrEstudiantes).forEach( estudiante => {
+        var str = estudiante.textContent;
+        var ele1 = str.toUpperCase();
+        var palabra = removeAccents(ele1);
+        if(palabra.includes(ele2) == false){
+            estudiante.style.display = "none";
+            contador++;
+        }else{
+            estudiante.style.display = "flex";
+        }
+    }
+    )
+    if(contador === limite){
+    
+        const urlObject = new URL(location.href);
+        var comprueba = urlObject.searchParams.get("L");
+        var mensaje;
+        var contenido;
+        var senal = document.createElement("h2");
+        if( comprueba == "EN"){
+            fetch("http://127.0.0.1:5500/reto5/conf/configEN.json")
+                    .then(function(res) {
+                        return res.json();
+                    })
+                    .then(function(data){
+                       mensaje = data.busqueda;
+                       mensaje = mensaje.replace("[query]", x);
+                       contenido = document.createTextNode(mensaje);
+                       senal.appendChild(contenido);
+                       senal.setAttribute("style", "color: rgb(41, 200, 199)")
+                       senal.setAttribute("id", "borrame")
+                       document.body.insertBefore(senal, document.getElementById("existencia"));
+                    })
+        } else if (comprueba == "PT"){
+            fetch("http://127.0.0.1:5500/reto5/conf/configPT.json")
+                    .then(function(res) {
+                        return res.json();
+                    })
+                    .then(function(data){
+                       mensaje = data.busqueda;
+                       mensaje = mensaje.replace("[query]", x);
+                       contenido = document.createTextNode(mensaje);
+                       senal.appendChild(contenido);
+                       senal.setAttribute("style", "color: rgb(41, 200, 199)")
+                       senal.setAttribute("id", "borrame")
+                       document.body.insertBefore(senal, document.getElementById("existencia"));
+                    })
+        }else {
+            fetch("http://127.0.0.1:5500/reto5/conf/configES.json")
+                    .then(function(res) {
+                        return res.json();
+                    })
+                    .then(function(data){
+                       mensaje = data.busqueda;
+                       mensaje = mensaje.replace("[query]", x);
+                       contenido = document.createTextNode(mensaje);
+                       senal.appendChild(contenido);
+                       senal.setAttribute("style", "color: rgb(41, 200, 199)")
+                       senal.setAttribute("id", "borrame")
+                       document.body.insertBefore(senal, document.getElementById("existencia"));
+                    })
+        }
+        
+        /*var senal = document.createElement("h2");
+        var mensaje = "No hay alumnos que tengan en su nombre: "
+         contenido = element.nombre;
+        var nombre = document.createTextNode(contenido)
+        var fuente = element.ci;
+        var nomfuente = element.imagen;
+        ima.setAttribute("src", "./reto5/"+nomfuente);
+        lis.appendChild(ima);
+        lis.appendChild(nombre);
+        lis.setAttribute("class", "estudiante");
+        document.getElementById("estudiantes").appendChild(lis);
+    }); */
+    }
+}
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  } 
